53aa5bae31c33414bfbc574eaa533a62fbf21422
Thomas <tc12game@gmail.com>
2024-03-12 19:44:17 -0400

    Changed port

--- my_website/server.js
+++ my_website/server.js
@@ -5,7 +5,7 @@ const path = require('path');
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000; // Set the port to 80
+const port = process.env.PORT || 80; // Set the port to 80
 app.listen(port, '0.0.0.0', () => {
     console.log(`Server is running on http://0.0.0.0:${port}`);
 });
