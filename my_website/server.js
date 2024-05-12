89f72560c830e9cd103651183b8acabefac483f2
Thomas <tc12game@gmail.com>
2024-05-12 18:33:39 -0400

    Testing

--- my_website/server.js
+++ my_website/server.js
@@ -1,12 +1,12 @@
-const express = require('express');
+/*const express = require('express');
 const app = express();
 const path = require('path');
-const hostname = '127.0.0.1';
 
+const hostname = '127.0.0.1';
+const port = 3000;
 // Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
 
-const port = process.env.PORT || 3000; 
 app.listen(port, hostname, () => {
     console.log(`Server is running on http://${hostname}:${port}`);
 });
@@ -29,4 +29,38 @@ app.get('/p3', (req, res) => {
 
 app.get('/p4', (req, res) => {
     res.sendFile(__dirname + '/p4/index.html');
+});*/
+
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(port, hostname, () => {
+    console.log(`Server is running on http://${hostname}:${port}`);
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/p1', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'p1', 'index.html'));
+});
+
+app.get('/p2', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'p2', 'index.html'));
+});
+
+app.get('/p3', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'p3', 'index.html'));
+});
+
+app.get('/p4', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'p4', 'index.html'));
 });
